fb9afd57321a666270a6cb6704f17c20c22a7d77
Rinat <o-roman@list.ru>
2023-10-15 15:20:57 +0500

    Задание спринт 7. Сдача № 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,88 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.NullPointerException
+import java.util.Scanner
+
+fun main() {
+    val archiveList = mutableListOf<Archive>()
+    val scanner = Scanner(System.`in`)
+    val menu = Menu()
+    var menuState = MenuState.ARCHIVE
+    var selectedArchive = -1
+    var selectedNote = -1
+    while (true) {
+        when (menuState) {
+            MenuState.ARCHIVE -> menu.render(archiveList, "Архив")
+            MenuState.NOTES -> menu.render(archiveList[selectedArchive].notes, "Заметку")
+            MenuState.VIEW -> {
+                if (selectedArchive >= 0 && selectedNote >= 0) {
+                    println(archiveList[selectedArchive].notes[selectedNote])
+                }
+                scanner.nextLine()
+                menuState = MenuState.NOTES
+                continue
+            }
+            MenuState.CREATE -> {
+                when {
+                    selectedArchive < 0 -> {
+                        println("Введите название архива")
+                        val name = scanner.nextLine()
+                        if (name.isEmpty()) {
+                            println("Название архива не может быть пустым")
+                            continue
+                        }
+                        archiveList.add(Archive(name, mutableListOf()))
+                        menuState = MenuState.ARCHIVE
+                        continue
+                    }
+                    selectedArchive >= 0 -> {
+                        println("Введите имя заметки")
+                        val name = scanner.nextLine()
+                        if (name.isEmpty()) {
+                            println("Название заметки не может быть пустым")
+                            continue
+                        }
+                        println("Введите текс заметки")
+                        val body = scanner.nextLine()
+                        if (body.isEmpty()) {
+                            println("Вы не ввели текст. Ну и ладно =(")
+                        }
+                        archiveList[selectedArchive].notes.add(Note(name, body))
+                        menuState = MenuState.NOTES
+                        continue
+                    }
+                }
+            }
+        }
+        val temp = scanner.nextLine()
+        var selectedIndex: Int = 99
+                for (i in temp) {
+                    if (!Character.isDigit(i) or temp.equals("")) {
+                        println("Вы не то ввели. Введите число.")
+                        continue
+                    }
+                    else selectedIndex = temp.toInt()
+                }
+        when {
+            selectedIndex == 0 -> menuState = MenuState.CREATE
+            selectedIndex < menu.menuItems.lastIndex -> {
+                when (menuState) {
+                    MenuState.ARCHIVE -> {
+                        selectedArchive = selectedIndex - 1
+                        menuState = MenuState.NOTES
+                    }
+                    MenuState.NOTES -> {
+                        selectedNote = selectedIndex - 1
+                        menuState = MenuState.VIEW
+                    }
+                    else -> { /* Ничего не делаем*/
+                    }
+                }
+            }
+            selectedIndex == menu.menuItems.lastIndex -> {
+                when (menuState) {
+                    MenuState.NOTES -> menuState = MenuState.ARCHIVE
+                    else -> return
+                }
+            }
+        }
+    }
 }
---
+++ src/main/kotlin/Menu.kt
@@ -0,0 +1,15 @@
+class Menu {
+    val menuFormatter = "%d. %s"
+    val menuItems = mutableListOf<MenuItem>()
+
+    fun render(list: List<File>, fileType: String) {
+        menuItems.clear()
+        menuItems.add(MenuItem("Создать ${fileType}"))
+        menuItems.addAll(list.map { MenuItem((it.name)) })
+        menuItems.add(MenuItem("Выход"))
+
+        menuItems.forEachIndexed { index, archiveMenuItem ->
+            println(String.format(menuFormatter, index, archiveMenuItem.title))
+        }
+    }
+}
